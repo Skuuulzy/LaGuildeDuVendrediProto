9a8f062f87ac4ba80022affb1bed563e2a3895d3
Skuuulzy <yona.rutkowski@gmail.com>
2024-01-05 17:05:30 +0100

    Linking Scene

--- VendrediProto/Assets/Scripts/Controllers/MainMenuController.cs
+++ VendrediProto/Assets/Scripts/Controllers/MainMenuController.cs
@@ -7,4 +7,13 @@ public class MainMenuController : MonoBehaviour
     {
         SceneLoader.OnLoadScene?.Invoke(SceneLoader.SceneIdentifier.CHARACTER_CONTROL);
     }
+    
+    public void LoadInteractionExampleScene()
+    {
+        SceneLoader.OnLoadScene?.Invoke(SceneLoader.SceneIdentifier.INTERACTION_EXAMPLE);
+    }
+    public void LoadMapTestScene()
+    {
+        SceneLoader.OnLoadScene?.Invoke(SceneLoader.SceneIdentifier.MAP_TEST);
+    }
 }
