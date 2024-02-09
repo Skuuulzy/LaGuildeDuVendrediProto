57971bf3db97c376c48c32b57211b020319a4410
iKeaght <noelmatheo@yahoo.fr>
2024-02-09 14:24:08 +0100

    Auto grid size
    Auto size grid and children based on value

--- VendrediProto/Assets/Ship/Scripts/GridManager.cs
+++ VendrediProto/Assets/Ship/Scripts/GridManager.cs
@@ -8,7 +8,8 @@ public class GridManager : MonoBehaviour
     [SerializeField] private int _height;
 
     [SerializeField] private Node _nodePrefab;
-
+    [SerializeField] private Transform _camera;
+    [SerializeField] private GameObject _gridContainer;
 
     private void Start()
     {
@@ -20,10 +21,17 @@ public class GridManager : MonoBehaviour
         {
             for(int z = 0; z < _height; z+= _nodePrefab._nodeSizeZ)
             {
-                Node spawnedNode = Instantiate(_nodePrefab, new Vector3(x, 0, z), Quaternion.identity);
-                spawnedNode.name = $"Node {x} {z}";
+                Node spawnedNode = Instantiate(_nodePrefab, new Vector3(x, 0, z), Quaternion.identity, _gridContainer.transform);
+                bool isOffset = (x%2 == 0 && z%2!=0) || (x % 2 != 0 && z % 2 == 0);
+                spawnedNode.name = $"Node {x}-{z}";
+
+                spawnedNode.Init(x, z, isOffset);
             }
         }
+        _camera.transform.position = new Vector3((float)_width / 2 - 0.5f, 100, (float)_height / 2 - 0.5f);
+        _camera.transform.Rotate(new Vector3(90, 0, 0)); 
     }
+
+
 
 }
--- VendrediProto/Assets/Ship/Scripts/Node.cs
+++ VendrediProto/Assets/Ship/Scripts/Node.cs
@@ -6,17 +6,25 @@ public class Node : MonoBehaviour
 {
 
     [SerializeField] public int _nodeSizeX;
-
     [SerializeField] public int _nodeSizeZ;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _xPosition;
+    [SerializeField] private float _zPosition;
+
+    //Debug
+    [SerializeField] private Color _baseColor;
+    [SerializeField] private Color _offsetColor;
+    [SerializeField] private Material _material;
+    [SerializeField] private Material _baseMaterial;
+    [SerializeField] private Material _offsetMaterial;
+
+    private const float _sizeRef = 10f;
+    public void Init(float xPosition, float zPosition, bool isOffset)
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        _xPosition = xPosition;
+        _zPosition = zPosition;
+        float calculatedSizeX = _nodeSizeX / _sizeRef;
+        float calculatedSizeZ = _nodeSizeZ / _sizeRef;
+        gameObject.transform.localScale = new Vector3(calculatedSizeX, 1, calculatedSizeZ);
+        _material = isOffset ? _offsetMaterial : _baseMaterial;
     }
 }
