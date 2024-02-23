a0a3377417004313940faac6542fbcff2268be96
Skuuulzy <yona.rutkowski@gmail.com>
2024-02-23 12:45:22 +0100

    Adding new singletons class

---
+++ VendrediProto/Assets/Component/Tools/Singletons/PersistentSingleton.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+namespace Component.Tools.Singletons
+{
+    /// <summary>
+    /// A singleton that will be persistent across scene by being a DDOL.
+    /// </summary>
+    public class PersistentSingleton<T> : MonoBehaviour where T : UnityEngine.Component
+    {
+        [Header("Singleton")]
+        [SerializeField] private bool _autoUnParentOnAwake = true;
+
+        private static T _instance;
+
+        public static bool HasInstance => _instance != null;
+        public static T TryGetInstance() => HasInstance ? _instance : null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindAnyObjectByType<T>();
+                    if (_instance == null)
+                    {
+                        var go = new GameObject(typeof(T).Name + " Auto-Generated");
+                        _instance = go.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Make sure to call base.Awake() in override if you need awake.
+        /// </summary>
+        protected virtual void Awake()
+        {
+            InitializeSingleton();
+        }
+
+        protected virtual void InitializeSingleton()
+        {
+            if (!Application.isPlaying) return;
+
+            if (_autoUnParentOnAwake)
+            {
+                transform.SetParent(null);
+            }
+
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                if (_instance != this)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ VendrediProto/Assets/Component/Tools/Singletons/Singleton.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace Component.Tools.Singletons
+{
+    /// <summary>
+    /// A simple singleton implementation.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Singleton<T> : MonoBehaviour where T : UnityEngine.Component
+    {
+        private static T _instance;
+
+        public static bool HasInstance => _instance != null;
+        public static T TryGetInstance() => HasInstance ? _instance : null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindAnyObjectByType<T>();
+                    if (_instance == null)
+                    {
+                        var go = new GameObject(typeof(T).Name + " Auto-Generated");
+                        _instance = go.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Make sure to call base.Awake() in override if you need awake.
+        /// </summary>
+        protected virtual void Awake()
+        {
+            InitializeSingleton();
+        }
+
+        protected virtual void InitializeSingleton()
+        {
+            if (!Application.isPlaying) return;
+
+            _instance = this as T;
+        }
+    }
+}
