e3c2edd27defe0aaf354b71a283df62463cf2a3a
Skuuulzy <yona.rutkowski@gmail.com>
2024-03-24 11:45:26 +0100

    Adding detection circle

--- VendrediProto/Assets/Component/UI/PlayerUI/ShipInfo/Scripts/PlayerUIShipsInfosController.cs
+++ VendrediProto/Assets/Component/UI/PlayerUI/ShipInfo/Scripts/PlayerUIShipsInfosController.cs
@@ -1,14 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using VComponent.Items.Merchandise;
 using VComponent.Ship;
 
 public class PlayerUIShipsInfosController : MonoBehaviour
 {
     [SerializeField] ShipInfoController _shipInfoControllerPrefab;
 	[SerializeField] private Transform _parentTransform;
-
 
 	/// <summary>
 	/// On one of our ship is created called this function to spawn an UI shipInfoController and view 
