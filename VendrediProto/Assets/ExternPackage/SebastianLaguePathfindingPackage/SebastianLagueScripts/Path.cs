1f5187004809c4271c919062b82218ca9c43828c
iKeaght <noelmatheo@yahoo.fr>
2024-04-09 19:19:26 +0200

    Several points of interest features

--- VendrediProto/Assets/ExternPackage/SebastianLaguePathfindingPackage/SebastianLagueScripts/Path.cs
+++ VendrediProto/Assets/ExternPackage/SebastianLaguePathfindingPackage/SebastianLagueScripts/Path.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace SebastianLague
@@ -7,19 +8,29 @@ namespace SebastianLague
 	public class Path 
 	{
 
-		public readonly Vector3[] LookPoints;
-		public readonly Line[] TurnBoundaries;
-		public readonly int FinishLineIndex;
+		public List<Vector3> LookPoints;
+		public Line[] TurnBoundaries;
+		public int FinishLineIndex;
 		public readonly int SlowDownIndex;
 
-		public Path(Vector3[] waypoints, Vector3 startPos, float turnDst, float stoppingDst) 
+		#region Constructors
+
+		public Path()
+		{
+			LookPoints = new List<Vector3>();
+			TurnBoundaries = new Line[0];
+			FinishLineIndex = 0;
+			SlowDownIndex = 0;
+		}
+
+		public Path(List<Vector3> waypoints, Vector3 startPos, float turnDst, float stoppingDst) 
 		{
 			LookPoints = waypoints;
-			TurnBoundaries = new Line[LookPoints.Length];
+			TurnBoundaries = new Line[LookPoints.Count];
 			FinishLineIndex = TurnBoundaries.Length - 1;
 			Vector2 previousPoint = V3ToV2 (startPos);
 
-			for (int i = 0; i < LookPoints.Length; i++) 
+			for (int i = 0; i < LookPoints.Count; i++) 
 			{
 				Vector2 currentPoint = V3ToV2 (LookPoints [i]);
 				Vector2 dirToCurrentPoint = (currentPoint - previousPoint).normalized;
@@ -30,7 +41,7 @@ namespace SebastianLague
 
 			float dstFromEndPoint = 0;
 
-			for (int i = LookPoints.Length - 1; i > 0; i--) 
+			for (int i = LookPoints.Count - 1; i > 0; i--) 
 			{
 				dstFromEndPoint += Vector3.Distance (LookPoints [i], LookPoints [i - 1]);
 
@@ -41,6 +52,9 @@ namespace SebastianLague
 				}
 			}
 		}
+
+
+		#endregion
 
 		Vector2 V3ToV2(Vector3 v3) 
 		{
--- VendrediProto/Assets/ExternPackage/SebastianLaguePathfindingPackage/SebastianLagueScripts/Unit.cs
+++ VendrediProto/Assets/ExternPackage/SebastianLaguePathfindingPackage/SebastianLagueScripts/Unit.cs
@@ -4,172 +4,194 @@ using UnityEditor.Timeline;
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using Sirenix.Utilities;
+using Unity.VisualScripting;
 
 namespace SebastianLague
 {
-	public class Unit : MonoBehaviour 
-	{
-		[SerializeField] private float _speed = 20;
-		[SerializeField] private float _turnSpeed = 3;
-		[SerializeField] private float _turnDst = 5;
-		[SerializeField] private float _stoppingDst = 10;
-		[SerializeField] private const float _minPathUpdateTime = .2f;
-		[SerializeField] private const float _pathUpdateMoveThreshold = .5f;
-		[SerializeField] private Transform _planeTransform;
-		[SerializeField] private float rotationResponsiveness = 0.3f;
-		[SerializeField] private PathfindingView _pathfindingView;
-		[SerializeField] private Transform _pathfindingPivot;
-		private Plane _plane;
-		private Vector3 _position;
-
+    public class Unit : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 20;
+        [SerializeField] private float _turnSpeed = 3;
+        [SerializeField] private float _turnDst = 5;
+        [SerializeField] private float _stoppingDst = 10;
+        [SerializeField] private const float _minPathUpdateTime = .2f;
+        [SerializeField] private const float _pathUpdateMoveThreshold = .5f;
+        [SerializeField] private Transform _planeTransform;
+        [SerializeField] private float rotationResponsiveness = 0.3f;
+        [SerializeField] private PathfindingView _pathfindingView;
+        [SerializeField] private Transform _pathfindingPivot;
+        private Plane _plane;
+        private Vector3 _position;
         private float _time;
         private float _timeDelay;
-		private bool _isInMovement = false;
+        private bool _isInMovement = false;
         private Path _path;
-		private List<Vector3> _pathWaypoints;
-		public float Speed => _speed;
-		public float TurnSpeed => _turnSpeed;
-		public float TurnDst => _turnDst;
-		public float StoppingDst => _stoppingDst;
+
+        //Multi paths
+        private bool _severalPointsOfInterest = false;
+        private Vector3 _secondPosition;
 
 
-		void Start() 
-		{
-			_position = transform.position;
-			_plane = new Plane(_planeTransform.up, _planeTransform.position);
+        private List<Vector3> _pathWaypoints;
+        public float Speed => _speed;
+        public float TurnSpeed => _turnSpeed;
+        public float TurnDst => _turnDst;
+        public float StoppingDst => _stoppingDst;
+
+
+        void Start()
+        {
+            _position = transform.position;
+            _plane = new Plane(_planeTransform.up, _planeTransform.position);
             _time = 0f;
             _timeDelay = 0.1f;
             StartCoroutine(UpdatePath());
-		}
+        }
 
-		private void Update()
-		{
-           
-            if (Input.GetMouseButton(0))
-			{
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				if (_plane.Raycast(ray, out var entry))
-				{
-					var position = ray.GetPoint(entry);
-					_position = position;
+        private void Update()
+        {
+
+            if (!Input.GetKey(KeyCode.LeftShift) && Input.GetMouseButton(0))
+            {
+
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (_plane.Raycast(ray, out var entry))
+                {
+                    var position = ray.GetPoint(entry);
+                    _position = position;
                     _pathfindingView.CreatePin(position);
-				}
-			}
+                }
+            }
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetMouseButton(0))
+            {
 
-			_time += Time.deltaTime;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (_plane.Raycast(ray, out var entry))
+                {
+                    var position = ray.GetPoint(entry);
+                    if (!_severalPointsOfInterest)
+                    {
+                        _position = position;
+                        _severalPointsOfInterest = true;
+                    }
+                    else
+                    {
+                        _secondPosition = position;
+                    }
+                    _pathfindingView.CreatePin(position);
+                }
+            }
+            _time += Time.deltaTime;
             if (_time >= _timeDelay && _isInMovement)
             {
-				_time = 0f;
-				_pathfindingView._pathFindingDrawer.DrawLines(_pathfindingPivot.position, _pathWaypoints);
-			}
-		}
+                _time = 0f;
+                _pathfindingView._pathFindingDrawer.DrawLines(_pathfindingPivot.position, _pathWaypoints);
+            }
+        }
 
-		public void OnPathFound(Vector3[] waypoints, bool pathSuccessful) 
-		{
-			if (pathSuccessful) 
-			{
-				_path = new Path(waypoints, transform.position, _turnDst, _stoppingDst);
-				_pathWaypoints = waypoints.ToList();
-				_pathfindingView._pathFindingDrawer.DrawLines(_pathfindingPivot.position, _pathWaypoints);
-                StopCoroutine("FollowPath");
-				StartCoroutine(FollowPath(() =>
-				{
-                    _pathfindingView.CleanPathfindingView();
-					_isInMovement = false;
-                }));
+        public void OnPathFound(Vector3[] waypoints, bool pathSuccessful)
+        {
+            if (pathSuccessful)
+            {
+                    _path = new Path(waypoints.ToList(), transform.position, _turnDst, _stoppingDst);
+                    _pathWaypoints = waypoints.ToList();
+                    _pathfindingView._pathFindingDrawer.DrawLines(_pathfindingPivot.position, _pathWaypoints);
+                    StopCoroutine("FollowPath");
+                    StartCoroutine(FollowPath(() =>
+                    {
+                        if (_severalPointsOfInterest)
+                        {
+                            PathRequestManager.RequestPath(new PathRequest(_path.LookPoints[_path.LookPoints.Count - 1], _secondPosition, OnPathFound));
+                        }
+                        _pathfindingView.CleanPathfindingView();
+                        _isInMovement = false;
+                        _severalPointsOfInterest = false;
+                    }));
+            }
+        }
 
-               
-			}
-		}
+        IEnumerator UpdatePath()
+        {
+            if (_position != null)
+            {
+                if (Time.timeSinceLevelLoad < .3f)
+                {
+                    yield return new WaitForSeconds(.3f);
+                }
 
-		IEnumerator UpdatePath() 
-		{
-			if(_position != null)
-			{
-				
-				if (Time.timeSinceLevelLoad < .3f)
-				{
-					yield return new WaitForSeconds(.3f);
-				}
+                PathRequestManager.RequestPath(new PathRequest(transform.position, _position, OnPathFound));
 
-				PathRequestManager.RequestPath(new PathRequest(transform.position, _position, OnPathFound));
+                float sqrMoveThreshold = _pathUpdateMoveThreshold * _pathUpdateMoveThreshold;
+                Vector3 targetPosOld = _position;
 
-				float sqrMoveThreshold = _pathUpdateMoveThreshold * _pathUpdateMoveThreshold;
-				Vector3 targetPosOld = _position;
+                while (true)
+                {
+                    yield return new WaitForSeconds(_minPathUpdateTime);
 
-				while (true)
-				{
-					yield return new WaitForSeconds(_minPathUpdateTime);
+                    if ((_position - targetPosOld).sqrMagnitude > sqrMoveThreshold)
+                    {
 
-					if ((_position - targetPosOld).sqrMagnitude > sqrMoveThreshold)
-					{
                         PathRequestManager.RequestPath(new PathRequest(transform.position, _position, OnPathFound));
-                        
                         targetPosOld = _position;
-					}
-				}
-			}
-		}
+                    }
+                }
+            }
 
-		IEnumerator FollowPath(Action isFinished) 
-		{
-			bool followingPath = true;
-			_isInMovement = true;
-			int pathIndex = 0;
-			Quaternion initialTargetRotation = Quaternion.LookRotation(_path.LookPoints[0] - transform.position);
-			transform.rotation = Quaternion.Lerp(transform.rotation, initialTargetRotation, rotationResponsiveness * Time.deltaTime);
-			float speedPercent = 1;
-            while (pathIndex < _path.TurnBoundaries.Length && followingPath) 
-			{
-				Vector2 pos2D = new Vector2 (transform.position.x, transform.position.z);
-				
-				
-                while ( _path.TurnBoundaries[pathIndex].HasCrossedLine(pos2D)) 
-				{
-					if (pathIndex == _path.FinishLineIndex) 
-					{
-						followingPath = false;
-						break;
-					} 
-					else 
-					{
-						pathIndex++;
+        }
 
-						if(_pathWaypoints.Count > 1)
-						{
-							_pathWaypoints.RemoveAt(0);
-						}
-					}
-				}
+        IEnumerator FollowPath(Action isFinished)
+        {
+            bool followingPath = true;
+            _isInMovement = true;
+            int pathIndex = 0;
+            Quaternion initialTargetRotation = Quaternion.LookRotation(_path.LookPoints[0] - transform.position);
+            transform.rotation = Quaternion.Lerp(transform.rotation, initialTargetRotation, rotationResponsiveness * Time.deltaTime);
+            float speedPercent = 1;
+            while (pathIndex < _path.TurnBoundaries.Length && followingPath)
+            {
+                Vector2 pos2D = new Vector2(transform.position.x, transform.position.z);
 
-				if (followingPath) 
-				{
-					if (pathIndex >= _path.SlowDownIndex && _stoppingDst > 0) 
-					{
-						speedPercent = Mathf.Clamp01(_path.TurnBoundaries [_path.FinishLineIndex].DistanceFromPoint(pos2D) / _stoppingDst);
 
-						if (speedPercent < 0.01f)
-						{
-							followingPath = false;
-						}
-					}
+                while (_path.TurnBoundaries[pathIndex].HasCrossedLine(pos2D))
+                {
+                    if (pathIndex == _path.FinishLineIndex)
+                    {
+                        followingPath = false;
+                        break;
+                    }
+                    else
+                    {
+                        pathIndex++;
+
+                        if (_pathWaypoints.Count > 1)
+                        {
+                            _pathWaypoints.RemoveAt(0);
+                        }
+                    }
+                }
+
+                if (followingPath)
+                {
+                    if (pathIndex >= _path.SlowDownIndex && _stoppingDst > 0)
+                    {
+                        speedPercent = Mathf.Clamp01(_path.TurnBoundaries[_path.FinishLineIndex].DistanceFromPoint(pos2D) / _stoppingDst);
+
+                        if (speedPercent < 0.01f)
+                        {
+                            followingPath = false;
+                        }
+                    }
                     Quaternion targetRotationPath = Quaternion.LookRotation(_path.LookPoints[pathIndex] - transform.position);
                     transform.rotation = Quaternion.Lerp(transform.rotation, targetRotationPath, Time.deltaTime * _turnSpeed);
-					transform.Translate(Vector3.forward * Time.deltaTime * Speed * speedPercent, Space.Self);
-				}
+                    transform.Translate(Vector3.forward * Time.deltaTime * Speed * speedPercent, Space.Self);
+                }
                 yield return null;
 
-			}
-			isFinished?.Invoke();
-		}
+            }
+            isFinished?.Invoke();
+        }
 
-		public void OnDrawGizmos() 
-		{
-			if (_path != null) 
-			{
-				_path.DrawWithGizmos();
-			}
-		}
-	}
+    }
 
 }
